88a73582db34a94e065d718058a391e3a074950e
Urbana28 <ana.urbana28@gmail.com>
2019-10-16 18:03:28 +0300

    Updates

---
+++ precache-manifest.895025c2f0ad38a38fac3520cd632b80.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9eb6bcb18b7d6dc35d3418db9ed78db",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "2cc815e717bc089bb7b8",
+    "url": "/portfolio/static/css/main.a3048b59.chunk.css"
+  },
+  {
+    "revision": "9c7bb41cb995022745b3",
+    "url": "/portfolio/static/js/2.d99cecf3.chunk.js"
+  },
+  {
+    "revision": "2cc815e717bc089bb7b8",
+    "url": "/portfolio/static/js/main.067861e8.chunk.js"
+  },
+  {
+    "revision": "b4d94345b62a965918a7",
+    "url": "/portfolio/static/js/runtime~main.d52ae8c2.js"
+  },
+  {
+    "revision": "fe269654640f8ef968fc0681e7ccdf11",
+    "url": "/portfolio/static/media/JS.fe269654.png"
+  },
+  {
+    "revision": "abfcfeefb7f518a2df619e9d62f93f9a",
+    "url": "/portfolio/static/media/facebook-logo.abfcfeef.svg"
+  },
+  {
+    "revision": "04557c1b0b790482dafb1bf4e082b69e",
+    "url": "/portfolio/static/media/react-hexagon.04557c1b.png"
+  },
+  {
+    "revision": "c38a3949787dc41b663417408933b3da",
+    "url": "/portfolio/static/media/vk-logo.c38a3949.svg"
+  }
+]);
